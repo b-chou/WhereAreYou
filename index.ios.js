eb77b8a11d8423329a3c1a3ff204e83a8b3bf9af
Ben Chou <ben@Bens-MacBook-Pro.local>
2016-05-23 16:44:15 -0700

    create groups

--- index.ios.js
+++ index.ios.js
@@ -16,8 +16,8 @@ import {
 const GOOGLEMAP_API_KEY = 'AIzaSyBOT8ZSLWLrh8aV-HJgkR20Lcc_tuTyyx0';
 const mongDB_API_KEY = 'wA1TEG2k7D3gXqsJ8SmM-FHmWiOsjkwU';
 const baseLink = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='
-var username = 'abc';
-var password = '123';
+var username, password, groupNameTemp, destinationTemp; 
+username = 'default';
 class AwesomeProject extends Component {
 
   constructor(props) {
@@ -30,6 +30,8 @@ class AwesomeProject extends Component {
       userObj: {},
       loaded: false,
       signedIn: false,
+      groupSelected: false,
+      groupCreateScreen: false
     };
   }
 
@@ -100,41 +102,6 @@ class AwesomeProject extends Component {
       .done();
   }
 
-  render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
-    if (!this.state.signedIn) {
-      return this.renderSignInView();
-    }
-
-    var eta = (this.state.etaObj);
-    return (
-      <View style={styles.container}>
-      <Text style={styles.thumbnail}>
-          Destination: {eta.destination}   
-      </Text>
-
-      <Text style={styles.thumbnail}>
-          {eta.miles} away
-      </Text>
-      <Text style={styles.thumbnail}>
-          {eta.time} away
-      </Text>
-      </View>
-    );
-  }
-
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading movies...
-        </Text>
-      </View>
-    );
-  }
   signIn() {
     var app = this;
     var accObj = {
@@ -175,6 +142,68 @@ class AwesomeProject extends Component {
     })
     .catch((e) => console.error(e));
   }
+
+  createGroupFlag() {
+    this.setState({
+      groupCreateScreen: true
+    });
+  }
+
+  createGroup() {
+    var groupObj = {
+      groupName: groupNameTemp,
+      creator: username,
+      destination: destinationTemp,
+      gropuMembers: []
+    }
+    fetch('https://api.mlab.com/api/1/databases/meetup/collections/Groups?apiKey=' + mongDB_API_KEY,
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(groupObj)
+      }).then(() => console.warn('Group Created'));
+    this.setState({
+      groupCreateScreen: false
+    });
+  }
+
+  viewGroups() {
+    fetch('https://api.mlab.com/api/1/databases/meetup/collections/Groups?apiKey=' + mongDB_API_KEY)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.error(responseData);
+ 
+    });
+  }
+
+  renderGroupCreateScreen() {
+    return(
+      <View>
+        <TextInput ref="groupName" onChangeText={(name) => groupNameTemp = name} style={[styles.searchInput, styles.Username]} placeholder='Group Name'/>
+        <TextInput ref="groupDestination" onChangeText={(name) => destinationTemp = name} style={[styles.searchInput, styles.Username, {marginTop: 20}]} placeholder='Destination'/>
+        <TouchableHighlight onPress={this.createGroup.bind(this)}> 
+        <Image style={[styles.button, {marginLeft: 85, width: 200, marginTop: 20}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Create+Group&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&w=212&h=50&c=15&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+
+  renderGroupSelect() {
+    return(
+      <View>
+        <TouchableHighlight onPress={this.createGroupFlag.bind(this)}> 
+        <Image style={[styles.button, {marginLeft:40, width: 300, marginTop: 200}]} source={{uri: 'http://dabuttonfactory.com/button.jpg?t=Create+a+Group&f=Caviar-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=33&vp=20&c=27&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=0&bc=569&shs=1&shc=444&sho=s'}}/>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.viewGroups.bind(this)}> 
+        <Image style={[styles.button, {marginLeft: 40, width: 300, marginTop: 200}]} source={{uri: 'http://dabuttonfactory.com/button.jpg?t=Search+for+a+Group&f=Caviar-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=33&vp=20&c=27&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=0&bc=569&shs=1&shc=444&sho=s'}}/>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+
   renderSignInView() {
     return (
       <View>
@@ -184,10 +213,57 @@ class AwesomeProject extends Component {
         <TextInput ref="username" onChangeText={(name) => username = name} style={[styles.searchInput, styles.Username]} placeholder='Username'/>
         <TextInput ref="password" onChangeText={(pass) => password = pass}style={[styles.searchInput, styles.Password]} placeholder='Password'/>
         <TouchableHighlight onPress={this.signIn.bind(this)}>
-        <Image style={styles.button} source={{uri: 'http://static1.squarespace.com/static/520b9d90e4b0db6a8088f152/t/5229f6e1e4b0fdd7a4e49392/1378481894683/Sign+In+Button.png'}}/>
+        <Image style={[styles.button]} source={{uri: 'http://static1.squarespace.com/static/520b9d90e4b0db6a8088f152/t/5229f6e1e4b0fdd7a4e49392/1378481894683/Sign+In+Button.png'}}/>
         </TouchableHighlight>
       </View>
     );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    if (!this.state.signedIn) {
+      return this.renderSignInView();
+    }
+
+    if (this.state.groupCreateScreen) {
+      return this.renderGroupCreateScreen();
+    }
+
+    if (!this.state.groupSelected) {
+      return this.renderGroupSelect();
+    }
+
+
+    if (etaSelected) {
+      var eta = (this.state.etaObj);
+      return (
+        <View style={styles.container}>
+        <Text style={styles.thumbnail}>
+            Destination: {eta.destination}   
+        </Text>
+
+        <Text style={styles.thumbnail}>
+            {eta.miles} away
+        </Text>
+        <Text style={styles.thumbnail}>
+            {eta.time} away
+        </Text>
+        </View>
+      );      
+    }
   }
 }
 
@@ -238,7 +314,7 @@ var styles = StyleSheet.create({
   },
   button: {
     width: 200,
-    height: 50,
+    height: 70,
     marginLeft: 90,
     marginTop: 30,
   },
