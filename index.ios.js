3484bf03275d3d9d27ae7c8386cb7d5af8d781e0
Ben Chou <ben@Bens-MacBook-Pro.local>
2016-05-24 16:45:33 -0700

    css added

--- index.ios.js
+++ index.ios.js
@@ -46,6 +46,27 @@ class AwesomeProject extends Component {
     };
   }
 
+  logout() {
+    this.setState({
+      userSignName: '',
+      userSignPass: '',
+      etaObj: {},
+      meetUpObj: {},
+      userObj: {},
+      currentGroup: {},
+      groupData: [],
+      groupDistanceData: [],
+      groupMemberDistanceFinalData: {},
+      focalDestination: '',
+      signedIn: false,
+      groupSelected: false,
+      groupCreateScreen: false,
+      groupViewScreen: false,
+      myGroupsScreen: false,
+      individualGroupScreen: false,
+    })
+  }
+
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -65,11 +86,6 @@ class AwesomeProject extends Component {
           longitude: position.coords.longitude        
         };
     });
-
-  }
-
-  componentLoop(){
-    this.getGeoLocation();
   }
 
 // google map functions
@@ -358,13 +374,18 @@ class AwesomeProject extends Component {
       groupList.push([t,i]);
     }
     return (
-    <View style = {styles.title}>
+      <Image source={{uri: 'http://www.codeproject.com/KB/mobile/439871/BackgroundImage.png'}} style={[styles.backgroundImage]}>
+      <View style={styles.title}>
           {groupList.map( (r) => {
             return <TouchableHighlight style={styles.buttonBox} onPress={() => this.joinGroup(r[1])}> 
             <Text style= {{marginTop: 5, marginLeft: 10, marginRight: 10}}>{r[0]} </Text>
             </TouchableHighlight>
           })}
-    </View>
+        <TouchableHighlight onPress={this.logout.bind(this)}> 
+        <Image style={[ styles.button, {marginLeft: -190, marginTop: -135, width: 300}, styles.logout]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Logout&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=68&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+        </TouchableHighlight>
+      </View>
+    </Image>
     );
   }
 
@@ -378,73 +399,81 @@ class AwesomeProject extends Component {
       }
     }
     return (
-    <View style = {styles.title}>
-      <Text>Your Groups</Text>
-          {groupList.map( (r) => {
-            return <TouchableHighlight style={styles.buttonBox} onPress={() => this.viewGroup(r[2], r[3])}> 
-            <Text style= {{marginTop: 10, marginLeft: 10, marginRight: 10}}>{r[0]} </Text>
-            </TouchableHighlight>
-          })}
-    </View>
+      <Image source={{uri: 'http://www.codeproject.com/KB/mobile/439871/BackgroundImage.png'}} style={styles.backgroundImage}>
+        <View style={styles.title}>
+          <TouchableHighlight onPress={this.logout.bind(this)}> 
+          <Image style={[ styles.button, {marginLeft: -190, marginTop: 300 , width: 300}, styles.logout]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Logout&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=68&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+          </TouchableHighlight>
+          <Text style={[styles.transparent, {marginBottom: 50, fontSize: 26, fontWeight: 'bold'}]}>Your Groups</Text>
+              {groupList.map( (r) => {
+                return <TouchableHighlight style={styles.buttonBox} onPress={() => this.viewGroup(r[2], r[3])}> 
+                <Text style= {{marginTop: 10, marginLeft: 10, marginRight: 10}}>{r[0]} </Text>
+                </TouchableHighlight>
+              })}
+        </View>
+      </Image>
     );
   }
 
   renderGroupCreateScreen() {
     return(
-      <View>
+      <Image source={{uri: 'http://www.codeproject.com/KB/mobile/439871/BackgroundImage.png'}} style={styles.backgroundImage}>
         <View style={styles.gpsContainer}>
-        <Text style={styles.gpsInfo}> ({userGPS.latitude}, {userGPS.longitude})</Text>
-        <Text style={styles.gpsInfo}> Neighborhood: {userGPS.neighborhood} </Text>
-        <Text style={styles.gpsInfo}> Approx Address: {userGPS.address} </Text>
+          <Text style={[styles.gpsInfo, styles.transparent]}> ({userGPS.latitude}, {userGPS.longitude})</Text>
+          <Text style={[styles.gpsInfo, styles.transparent]}> Neighborhood: {userGPS.neighborhood} </Text>
+          <Text style={[styles.gpsInfo, styles.transparent]}> Approx Address: {userGPS.address} </Text>
+          <TextInput ref="groupName" onChangeText={(name) => groupNameTemp = name} style={[styles.searchInput, styles.Username, {marginTop: 70}]} placeholder='Group Name'/>
+          <TextInput ref="groupDestination" onChangeText={(name) => destinationTemp = name} style={[styles.searchInput, styles.Username, {marginTop: 20}]} placeholder='Destination'/>
+          <TouchableHighlight onPress={this.createGroup.bind(this)}> 
+          <Image style={[styles.button, {marginLeft: 85, width: 200, marginTop: 20}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Create+Group&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&w=212&h=50&c=15&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={this.logout.bind(this)}> 
+          <Image style={[ styles.button, {marginLeft: 0, marginTop: 70, width: 300}, styles.logout]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Logout&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=68&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+          </TouchableHighlight>
         </View>
-        <TextInput ref="groupName" onChangeText={(name) => groupNameTemp = name} style={[styles.searchInput, styles.Username]} placeholder='Group Name'/>
-        <TextInput ref="groupDestination" onChangeText={(name) => destinationTemp = name} style={[styles.searchInput, styles.Username, {marginTop: 20}]} placeholder='Destination'/>
-        <TouchableHighlight onPress={this.createGroup.bind(this)}> 
-        <Image style={[styles.button, {marginLeft: 85, width: 200, marginTop: 20}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Create+Group&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&w=212&h=50&c=15&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
-        </TouchableHighlight>
-      </View>
+      </Image>
     );
   }
 
   renderGroupSelect() {
     return(
-      <View>
+      <Image source={{uri: 'http://www.codeproject.com/KB/mobile/439871/BackgroundImage.png'}} style={styles.backgroundImage}>
         <View style={styles.gpsContainer}>
-        <Text style={styles.gpsInfo}> ({userGPS.latitude}, {userGPS.longitude})</Text>
-        <Text style={styles.gpsInfo}> Neighborhood: {userGPS.neighborhood} </Text>
-        <Text style={styles.gpsInfo}> Approx Address: {userGPS.address} </Text>
-        </View>
+        <Text style={[styles.gpsInfo, styles.transparent]}> ({userGPS.latitude}, {userGPS.longitude})</Text>
+        <Text style={[styles.gpsInfo, styles.transparent]}> Neighborhood: {userGPS.neighborhood} </Text>
+        <Text style={[styles.gpsInfo, styles.transparent]}> Approx Address: {userGPS.address} </Text>
         <TouchableHighlight onPress={this.createGroupFlag.bind(this)}> 
-        <Image style={[styles.button, {marginLeft:40, width: 300, marginTop: 100}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Create+a+Group&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=31&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+        <Image style={[styles.button, {marginLeft:40, width: 300}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Create+a+Group&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=31&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
         </TouchableHighlight>
         <TouchableHighlight onPress={this.viewGroupsFlag.bind(this)}> 
-        <Image style={[styles.button, {marginLeft: 40, width: 300, marginTop: 100}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Search+for+a+Group&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=31&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+        <Image style={[styles.button, {marginLeft: 40, width: 300}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Search+for+a+Group&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=31&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
         </TouchableHighlight>
         <TouchableHighlight onPress={this.viewMyGroupsFlag.bind(this)}> 
-        <Image style={[styles.button, {marginLeft: 40, width: 300, marginTop: 100}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=View+My+Groups&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=31&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+        <Image style={[styles.button, {marginLeft: 40, width: 300}]} source={{uri: 'http://dabuttonfactory.com/button.png?t=View+My+Groups&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=31&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
         </TouchableHighlight>
-      </View>
+        <TouchableHighlight onPress={this.logout.bind(this)}> 
+        <Image style={[ styles.button, {marginLeft: 0, width: 300}, styles.logout]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Logout&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=68&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+        </TouchableHighlight>
+        </View>
+      </Image>
     );
   }
 
   renderSignInView() {
     return (
-      <View>
+      <Image source={{uri: 'http://www.codeproject.com/KB/mobile/439871/BackgroundImage.png'}} style={styles.backgroundImage}>
         <View style={styles.gpsContainer}>
-        <Text style={styles.gpsInfo}> ({userGPS.latitude}, {userGPS.longitude})</Text>
-        <Text style={styles.gpsInfo}> Neighborhood: {userGPS.neighborhood} </Text>
-        <Text style={styles.gpsInfo}> Approx Address: {userGPS.address} </Text>
-        </View>
-        <Text style={[styles.signIn, styles.title]}>
-        User Sign In
-        </Text>
-
-        <TextInput ref="username" onChangeText={(name) => username = name} style={[styles.searchInput, styles.Username]} placeholder='Username'/>
-        <TextInput ref="password" onChangeText={(pass) => password = pass}style={[styles.searchInput, styles.Password]} placeholder='Password'/>
+        <Text style={[styles.gpsInfo, styles.transparent]}> ({userGPS.latitude}, {userGPS.longitude})</Text>
+        <Text style={[styles.gpsInfo, styles.transparent]}> Neighborhood: {userGPS.neighborhood} </Text>
+        <Text style={[styles.gpsInfo, styles.transparent]}> Approx Address: {userGPS.address} </Text>
+        <Text style={[styles.transparent, styles.mainTitle]}>Where are you?</Text>
+        <TextInput ref="username" onChangeText={(name) => username = name} style={[styles.searchInput, styles.Username, {marginTop: 200}]} placeholder='Username'/>
+        <TextInput ref="password" onChangeText={(pass) => password = pass} style={[styles.searchInput, styles.Password]} placeholder='Password'/>
         <TouchableHighlight onPress={this.signIn.bind(this)}>
         <Image style={[styles.button]} source={{uri: 'http://static1.squarespace.com/static/520b9d90e4b0db6a8088f152/t/5229f6e1e4b0fdd7a4e49392/1378481894683/Sign+In+Button.png'}}/>
         </TouchableHighlight>
-      </View>
+        </View>
+      </Image>
     );
   }
 
@@ -460,13 +489,19 @@ class AwesomeProject extends Component {
 
   renderIndividualGroupScreen() {
     return (
-      <View>
-        <Text style={styles.title}> {this.state.currentGroup.groupName} </Text>
-        <Text style={[styles.title, {marginTop: 30, marginBottom: 100}]}> Destination: {this.state.currentGroup.destination} </Text>
+      <Image source={{uri: 'http://www.codeproject.com/KB/mobile/439871/BackgroundImage.png'}} style={styles.backgroundImage}>
+        <Text style={[styles.title, styles.transparent]}> {this.state.currentGroup.groupName} </Text>
+        <Text style={[styles.title, styles.transparent, {marginTop: 30, marginBottom: 100}]}> Destination: {this.state.currentGroup.destination} </Text>
+        <Text style={[styles.transparent, {marginBottom: 10, marginLeft: 5, fontSize: 16}]}>Members </Text>
         {this.state.groupDistanceData.map((member) => {
-          return <Text style={{marginLeft: 20, marginBottom: 20, fontSize: 16}}>Member: {member.username}, {member.miles} away, ETA: {member.time}</Text>
+          return <Text style={[{marginLeft: 20, marginBottom: 20, fontSize: 16}, styles.transparent]}>{member.username} | {member.miles} away | ETA: {member.time}</Text>
         })}
-      </View>
+
+        <TouchableHighlight onPress={this.logout.bind(this)}> 
+        <Image style={[ styles.button, {marginLeft: 0, marginTop: -30, width: 300}, styles.logout]} source={{uri: 'http://dabuttonfactory.com/button.png?t=Logout&f=Calibri-Bold&ts=24&tc=fff&tshs=1&tshc=000&hp=68&vp=17&c=8&bgt=gradient&bgc=3d85c6&ebgc=073763&bs=1&bc=569&shs=1&shc=444&sho=s'}}/>
+        </TouchableHighlight>
+
+      </Image>
     );
   }
 
@@ -521,6 +556,19 @@ class AwesomeProject extends Component {
 }
 
 var styles = StyleSheet.create({
+  transparent: {
+    backgroundColor: 'transparent',
+  },
+  mainTitle: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    top: 150,
+  },
+  backgroundImage: {
+    flex:1,
+    resizeMode: 'cover'
+  },
   container: {
     flex: 1,
     flexDirection: 'row',
@@ -558,35 +606,37 @@ var styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   searchInput: {
-    marginTop: 100,
-    height: 36,
-    padding: 4,
-    marginRight: 80,
+    textAlign: 'center',
     marginLeft: 80,
-    flex: 2,
-    fontSize: 18,
+    height: 40,
+    width: 200,
     borderWidth: 1,
-    borderColor: '#48BBEC',
+    borderColor: 'black',
     borderRadius: 8,
-    color: '#48BBEC'
+    color: 'black'
   },
   signIn: {
-    marginTop: 200,
+    marginTop: -100,
   },
   button: {
     width: 200,
     height: 70,
-    marginLeft: 90,
     marginTop: 30,
+    marginLeft: 90,
   },
   Password: {
     marginTop: 20,
+  },
+  logout: {
+    position: "absolute",
+    left: 40,
+    top: 160
   },
   buttonBox: {
     width: 200,
     marginTop: 10,
     marginBottom: 10,
-    height: 50,
+    height: 70,
     borderBottomWidth: 2,
     borderLeftWidth: 2,
     borderTopWidth: 2,
